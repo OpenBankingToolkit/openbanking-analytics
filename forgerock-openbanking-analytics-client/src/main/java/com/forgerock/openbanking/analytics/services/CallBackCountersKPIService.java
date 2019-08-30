b366815c0887960dadb73d85699be30f24c940e2
quentin.castel <quentin.castel@forgerock.com>
2019-08-30 15:44:01 +0100

    Missing metrics classes

---
+++ forgerock-openbanking-analytics-client/src/main/java/com/forgerock/openbanking/analytics/services/CallBackCountersKPIService.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright 2019 ForgeRock AS. All Rights Reserved
+ *
+ * Use of this code requires a commercial software license with ForgeRock AS.
+ * or with one of its affiliates. All use shall be exclusively subject
+ * to such license between the licensee and ForgeRock AS.
+ */
+package com.forgerock.openbanking.analytics.services;
+
+import brave.Tracer;
+import com.forgerock.openbanking.analytics.configuration.MetricsConfiguration;
+import com.forgerock.openbanking.analytics.model.entries.callback.CallBackCounterEntry;
+import com.forgerock.openbanking.analytics.utils.MetricUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@Slf4j
+@Service
+public class CallBackCountersKPIService {
+
+    private final RestTemplate restTemplate;
+    private final MetricsConfiguration metricsConfiguration;
+    private final Tracer tracer;
+
+    public CallBackCountersKPIService(RestTemplate restTemplate, MetricsConfiguration metricsConfiguration, Tracer tracer) {
+        this.restTemplate = restTemplate;
+        this.metricsConfiguration = metricsConfiguration;
+        this.tracer = tracer;
+    }
+
+    public void sendCallBackEntries(List<CallBackCounterEntry> callBackCounterEntries) {
+        if (!MetricUtils.isRequestEnabledForAnalytics(tracer)) {
+            log.debug("Request excluded for analytics");
+            return;
+        }
+        log.debug("Send call back entries {}", callBackCounterEntries);
+        try {
+            restTemplate.postForEntity(metricsConfiguration.rootEndpoint + "/api/kpi/callbacks/", callBackCounterEntries, Void.class);
+        } catch (Exception e) {
+            log.warn("Couldn't send callbacks {} KPI to metrics", callBackCounterEntries, e);
+        }
+    }
+}
