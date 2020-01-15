929b1a825377e572fe646764e1d6c5142c78fe1f
Julien Renaux <1388706+shprink@users.noreply.github.com>
2020-01-15 09:19:20 +0100

    cleanup themes (#61)

--- forgerock-openbanking-ui/themes/bendigobank/deployment-settings.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  defaultSettings: {
-    client: {
-      name: 'Bendigo Bank'
-    }
-  }
-};
--- forgerock-openbanking-ui/themes/suncorp/deployment-settings.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  defaultSettings: {
-    client: {
-      name: 'Suncorp'
-    }
-  }
-};
